cbc597a7799c58e61997668d8d3601e03cbd90b4
Cezar Neaga <hi@cezarneaga.eu>
2019-11-29 18:31:14 +0100

    get the initial setup up and running

---
+++ gatsby-config.js
@@ -0,0 +1,49 @@
+// @ts-check
+const siteConfig = require('./site-config')
+
+module.exports = {
+  siteMetadata: {
+    ...siteConfig,
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
+    // 'gatsby-transformer-json',
+    // 'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-typescript',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        // eslint-disable-next-line
+        short_name: `Servicii`,
+        // eslint-disable-next-line
+        start_url: `/`,
+        // eslint-disable-next-line
+        background_color: `#663399`,
+        // eslint-disable-next-line
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/harta-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-netlify-cache`,
+      options: {
+        // this may cause builds to fail in the long run
+        cachePublic: true,
+      },
+    },
+  ],
+}
---
+++ site-config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  siteTitle: 'New Paradigm Ventures',
+  siteTitleShort: 'NPV',
+  siteDescription: 'Building the social innovation and impact finance field',
+  siteUrl: 'https://npv.netlify.com',
+  themeColor: '#000',
+  backgroundColor: '#fff',
+  pathPrefix: null,
+  logo: path.resolve(__dirname, 'src/images/icon.png'),
+  social: {
+    twitter: 'nikolaushutter',
+    fbAppId: '123',
+  },
+}
