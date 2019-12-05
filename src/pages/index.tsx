ada11378ced522dbb766ca4db6b4e82af490be65
Cezar Neaga <hi@cezarneaga.eu>
2019-12-05 23:11:22 +0100

    index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
-
+import { Facebook } from 'react-feather'
 import Layout from '../components/Layout'
-import MainButton from '../components/MainButton'
+import { AlternativeButton, DetailsButton, FilterButton, MainButton, MoreButton, SocialButton } from '../components/buttons'
 export default function Index() {
   const test = () => console.log('click')
   return (
     <Layout>
+      <br />
+      <br />
+      <FilterButton label="Copii" count={6} />
+      <br />
+      <SocialButton action={test} label="Comunitate">
+        <Facebook
+          size={31}
+          strokeWidth={1.5}
+          style={{
+            padding: '5.5px',
+            border: 'solid 1.5px #fff',
+            borderRadius: 26,
+            margin: '-5.5px',
+          }}
+        />
+      </SocialButton>
+      <br />
+      <MoreButton action={test} />
+      <br />
+      <DetailsButton action={test} />
+      <br />
+      <AlternativeButton action={test} />
+      <br />
       <MainButton label="Deschide harta" action={test} />
     </Layout>
   )
