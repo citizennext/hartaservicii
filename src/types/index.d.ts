e6cd1536804cd915ffe15e53ee5fea399bc08da3
Cezar Neaga <hi@cezarneaga.eu>
2019-12-05 23:11:02 +0100

    types and theme presentation page

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,48 @@
 declare module '@theme-ui/components' {
   const Button: any
+  const IconButton: any
   const Text: any
-  export { Button, Text }
+
+  export { Button, Text, IconButton }
+}
+
+declare module '*.bmp' {
+  const src: string
+  export default src
+}
+
+declare module '*.gif' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpg' {
+  const src: string
+  export default src
+}
+
+declare module '*.jpeg' {
+  const src: string
+  export default src
+}
+
+declare module '*.png' {
+  const src: string
+  export default src
+}
+
+declare module '*.webp' {
+  const src: string
+  export default src
+}
+
+declare module '*.svg' {
+  import * as React from 'react'
+
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<
+    SVGSVGElement
+  >>
+
+  const src: string
+  export default src
 }
