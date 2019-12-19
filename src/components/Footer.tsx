b56042308637c6e714a8addf83e19650e00f09aa
Catalin Secosan <secosan@gmail.com>
2019-12-19 15:09:18 +0200

    footer structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,29 @@
 /** @jsx jsx */
 import React from 'react'
 import {jsx} from '@emotion/core'
+import logo_light from '../assets/images/logo_light.svg'
 
 export default class Footer extends React.Component {
+
     render() {
         return (
-            <footer>
-                <div>Footer</div>
-            </footer>
+                <footer>
+					<img src={logo_light} />
+                	<p>este un proiect realizat de <a href="https://citizennext.ro/contribute">Asociația Citizen Next</a> cu</p>
+                	<p>sprijinul <a href="https://www.omvpetrom.com/ro">OMV Petrom</a> prin programul <a href="https://taraluiandrei.ro/">Țara lui Andrei</a>.</p>
+
+                	<ul>
+						<li><a href="…">Harta</a></li>
+						<li><a href="…">Despre noi</a></li>
+						<li><a href="…">Noutăți</a></li>
+						<li><a href="…">Parteneri</a></li>
+						<li><a href="…">Statistici</a></li>
+						<li><a href="…">Contact</a></li>
+					</ul>
+
+					<p>Asociația Citizen Next — toate drepturile rezervate © {(new Date().getFullYear())}</p>
+
+                </footer>
         );
     }
 }
