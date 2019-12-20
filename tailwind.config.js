c6894d2e28fb655c09fbb383bed50c3bdca8aa4f
Catalin Secosan <secosan@gmail.com>
2019-12-20 12:53:45 +0200

    cleaned up footer a little

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@ module.exports = {
   theme: {
     colors: {
       celeste: '#6FBBB7',
+      brown: '#877272',
+      lightbrown: '#C4C3C3',
+      snow: '#EDF7EF',
     },
-
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {}
   },
   variants: {},
