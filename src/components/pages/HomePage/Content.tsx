e6b705a06db5e574c53c8403b82e0e6bcb3dfb03
Catalin Secosan <secosan@gmail.com>
2020-01-06 16:27:34 +0200

    moved design examples to test

--- src/components/pages/HomePage/Content.tsx
+++ src/components/pages/HomePage/Content.tsx
@@ -1,32 +1,45 @@
 /** @jsx jsx */
 import React from 'react'
 import { jsx } from '@emotion/core'
+import Data from '../../../data/global.json'
+import hero from Data.page.homepage.images.intro.src
 
 export default class Content extends React.Component {
   render() {
     return (
       <div>
-        <h1>Îngrijim și suntem îngrijiți mai bine</h1>
-        <br />
-        <h2>H2 Title</h2>
-        <br />
-        <h3>H3 Title</h3>
-        <br />
-        <h4>H4 Title</h4>
-        <br />
-        <p>Paragraph</p>
-        <br />
-        <p className="notes">Notes</p>
-        <br />
-        <a className="mini">a mini</a>
-        <br />
-        <button>Button</button>
-        <br />
-        <button className="social">Button</button>
-        <br />
-        <div>Home Page Content</div>
-        <br />
-        <a href="/test">TEST</a>
+        <div id="hero">
+          <h1>{Data.page.homepage.content.title}</h1>
+          <p>{Data.page.homepage.content.intro}</p>
+          <button className="small" formAction={Data.page.homepage.links.intro.url} title={Data.page.homepage.links.intro.title}>
+            {Data.page.homepage.links.intro.label}
+          </button>
+          <img src={hero} />
+        </div>
+        <div id="section2" className="md:flex">
+          <div className="section-list  md:w-1/2">
+            <img src="../../../assets/images/hp_hero.png" />
+            <h3>{Data.page.homepage.content.section2.title}</h3>
+            <ol className="list-decimal">
+              <li>{Data.page.homepage.content.section2.list1}</li>
+              <li>{Data.page.homepage.content.section2.list2}</li>
+              <li>{Data.page.homepage.content.section2.list3}</li>
+            </ol>
+            <button>{Data.page.homepage.links.section2.label}</button>
+          </div>
+          <div className="section-list md:w-1/2">
+            <img src="../${Data.page.homepage.images.section2.src}" />
+            <h3 className="border-transparent border-b-4">{Data.page.homepage.content.section3.title}</h3>
+            <ol>
+              <li>{Data.page.homepage.content.section3.list1}</li>
+              <li>{Data.page.homepage.content.section3.list2}</li>
+              <li>{Data.page.homepage.content.section3.list3}</li>
+            </ol>
+            <button>{Data.page.homepage.links.section3.label}</button>
+          </div>
+        </div>
+
+        <button onClick="/test">TEST</button>
       </div>
     )
   }
--- src/components/pages/Test/Content.tsx
+++ src/components/pages/Test/Content.tsx
@@ -15,6 +15,25 @@ export default class Content extends React.Component {
   render() {
     return (
       <div>
+        <h1>H1 Title</h1>
+        <br />
+        <h2>H2 Title</h2>
+        <br />
+        <h3>H3 Title</h3>
+        <br />
+        <h4>H4 Title</h4>
+        <br />
+        <p>Paragraph</p>
+        <br />
+        <p className="notes">Notes</p>
+        <br />
+        <a className="mini">a mini</a>
+        <br />
+        <button>Button</button>
+        <br />
+        <button className="social">Button</button>
+        <br />
+        <button className="small">Small Button</button>
         <div>{Data.page.test.content}</div>
         <br />
         <a href={Data.page.test.links.homepage.url} title={Data.page.test.links.homepage.title}>
