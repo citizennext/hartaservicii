c0748e00b9b4db936f9c1ed25ad6473683f065d2
Catalin Secosan <secosan@gmail.com>
2020-01-07 18:48:03 +0200

    progress baf refactor

--- src/components/CircularProgressBar/index.tsx
+++ src/components/CircularProgressBar/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
 
 export default class CircularProgressBar extends React.Component {
-  //constructor(props) {
-  //    super(props);
-  //    this.state = {};
-  //}
-
   render() {
     // Size of the enclosing square
     const sqSize = this.props.sqSize
@@ -45,21 +40,43 @@ export default class CircularProgressBar extends React.Component {
               strokeDashoffset: dashOffset,
             }}
           />
-          <text className="circle-private" x="20%" y="50%" dy=".3em" textAnchor="middle">
-            {`${this.props.private}`} private
+          <text className="circle-private" x="25%" y="45%" dy=".3em" textAnchor="middle">
+            {`${this.props.private}`}
           </text>
-          <text className="circle-publice" x="80%" y="50%" dy=".3em" textAnchor="middle">
-            {`${this.props.publice}`} publice
+          <text x="15%" y="55%" className="block text-xl text-black">
+            private
+          </text>
+          <text className="circle-publice" x="75%" y="45%" dy=".3em" textAnchor="middle">
+            {`${this.props.publice}`}
+          </text>
+          <text x="65%" y="55%" className="block text-xl text-black">
+            publice
           </text>
         </svg>
-        <p className="circle-publice"> Total {`${total}`} Centre servicii sociale acreditate pe tot teritoriul țării</p>
+        <svg
+          width="272"
+          height="67"
+          viewBox="0 0 272 67"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="-mt-10 m-auto">
+          <path
+            d="M2.92 2.58984C19.4702 21.6343 39.9151 36.9048 62.8735 47.3697C85.832 57.8345 110.769 63.2498 136 63.2498C161.231 63.2498 186.168 57.8345 209.126 47.3697C232.085 36.9048 252.53 21.6343 269.08 2.58984"
+            stroke="#968584"
+            strokeWidth="7"
+            strokeMiterlimit="10"
+          />
+        </svg>
+        <p className="circle-total pt-4">TOTAL</p>
+        <p className="circle-total-amount">{`${total}`}</p>
+        <p className="circle-total pb-6">Centre servicii sociale acreditate pe tot teritoriul țării</p>
       </div>
     )
   }
 }
 
 CircularProgressBar.defaultProps = {
-  sqSize: 200,
+  sqSize: 310,
   publice: 1396,
   private: 1154,
   strokeWidth: 30,
