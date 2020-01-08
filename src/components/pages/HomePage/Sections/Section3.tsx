9bbba2efb2cd6626b7d18b9e459bfa90b7fbc8cb
Catalin Secosan <secosan@gmail.com>
2020-01-08 18:02:49 +0200

    HP Sections refactor

--- src/components/pages/HomePage/Sections/Section3.tsx
+++ src/components/pages/HomePage/Sections/Section3.tsx
@@ -7,9 +7,11 @@ export default class Content extends React.Component {
     return (
       <div id="section3" className="section circularprogressbar">
         <div className="interior bg-snow py-8 mb-16">
-          <CircularProgressBar publice={1396} private={1154} />
-          <ProgressBar adapost={21} copii={538} />
-          <button className="my-1 absolute">Toate statisticile</button>
+          <div className="md:flex">
+            <CircularProgressBar class="md:w-1/2" publice={1396} private={1154} />
+            <ProgressBar class="md:w-1/2" adapost={21} copii={538} />
+          </div>
+          <button className="my-1 absolute md:left-1/4">Toate statisticile</button>
         </div>
       </div>
     )
--- src/components/pages/HomePage/Sections/Section4.tsx
+++ src/components/pages/HomePage/Sections/Section4.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import echipa from '../../../../assets/images/echipa.png'
 import BlogPost from '../../../BlogPost'
 
 export default class Content extends React.Component {
@@ -7,11 +6,12 @@ export default class Content extends React.Component {
     return (
       <div id="section4" className="section blogposts bg-white">
         <div className="interior mb-16">
-          <img className="border-b-8 border-burg mb-4" src={echipa} />
-          <BlogPost />
-          <BlogPost />
-          <BlogPost />
-          <button className="my-4">Toate noutățile</button>
+          <div className="md:flex">
+            <BlogPost />
+            <BlogPost />
+            <BlogPost class="md:hidden xl:block" />
+          </div>
+          <button className="my-4 md:left-1/4">Toate noutățile</button>
         </div>
       </div>
     )
