619d1cfeb46623e8424b14d92dc86071ec2aed42
St3phan <office@green-art.ro>
2020-01-08 17:47:59 +0200

    implement the hasura graphql, implemented data from hasura on leaflet map, refactoring and fixed issues

--- src/components/pages/Harta/Content.tsx
+++ src/components/pages/Harta/Content.tsx
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
-import Providers from './providers.json'
+import React, { Component } from 'react';
+import { Map, Marker, TileLayer } from 'react-leaflet';
+import { LatLngTuple } from 'leaflet';
+import { graphql, StaticQuery } from 'gatsby';
 
 type State = {
-  lat: number
-  lng: number
-}
+  lat: number;
+  lng: number;
+};
 
 export default class SimpleExample extends Component<{}, State> {
   state = {
@@ -13,22 +14,42 @@ export default class SimpleExample extends Component<{}, State> {
     lng: 25.091419,
     zoom: 7,
     scrollWheelZoom: false,
-  }
+    pane: 'markerPane',
+  };
 
   render() {
-    const position = [this.state.lat, this.state.lng]
+    const position: LatLngTuple = [this.state.lat, this.state.lng];
+    const query = graphql`
+      query {
+        hasura {
+          providers {
+            id
+            coordinates
+            name
+          }
+        }
+      }
+    `;
+
     return (
-      <Map center={position} zoom={this.state.zoom} scrollWheelZoom={this.state.scrollWheelZoom}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {Object.values(Providers).map(item => (
-          <Marker position={[item.lat, item.lng]} key={1}>
-            <Popup>{[item.popup]}</Popup>
-          </Marker>
-        ))}
-      </Map>
-    )
+      <StaticQuery
+        query={query}
+        render={data => {
+          const providers = data.hasura.providers;
+
+          return (
+            <Map center={position} zoom={this.state.zoom} scrollWheelZoom={this.state.scrollWheelZoom}>
+              <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              {Object.values(providers).map(item => (
+                <Marker position={item.coordinates} key={1} />
+              ))}
+            </Map>
+          );
+        }}
+      />
+    );
   }
 }
