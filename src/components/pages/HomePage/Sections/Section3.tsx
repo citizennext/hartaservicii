34b5166871c7b2f589d374b60363f11db3e15545
Cezar Neaga <hi@cezarneaga.eu>
2020-01-08 14:16:08 +0100

    refactor to functional component

--- src/components/pages/HomePage/Sections/Section3.tsx
+++ src/components/pages/HomePage/Sections/Section3.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import CircularProgressBar from '../../../CircularProgressBar'
-import ProgressBar from '../../../ProgressBar'
+// import ProgressBar from '../../../ProgressBar'
 
 export default class Content extends React.Component {
   render() {
     return (
       <div id="section3" className="section circularprogressbar">
         <div className="interior bg-snow py-8 mb-16">
-          <CircularProgressBar publice={1396} private={1154} />
-          <ProgressBar adapost={21} copii={538} />
+          <CircularProgressBar public={1396} private={1154} />
+          {/* <ProgressBar adapost={21} copii={538} /> */}
           <button className="my-1 absolute">Toate statisticile</button>
         </div>
       </div>
