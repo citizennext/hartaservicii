9029f9c04815e1edadd55a3d75ad0214f52bdce1
Catalin Secosan <secosan@gmail.com>
2020-01-09 19:09:36 +0200

    refactored footer for desktop and ipad

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,71 +1,44 @@
 /** @jsx jsx */
-import React from 'react'
-import { jsx } from '@emotion/core'
-import { Link } from 'gatsby'
-import Data from '../data/global.json'
-import logoLight from '../assets/images/logo_light.svg'
+import React from 'react';
+import { jsx } from '@emotion/core';
+import { Link } from 'gatsby';
+import Data from '../data/global.json';
+import logoLight from '../assets/images/logo_light.svg';
+import Navigation from './Navigation';
+import Separator from './Separator';
 
 export default class Footer extends React.Component {
   render() {
     return (
       <footer className="px-6">
-        <div className="border-celeste border-t-8 md:flex md:flex-wrap">
-          <div>
-            <img alt="Harta Serviciilor Sociale Logo" src={logoLight} className="pt-8" />
-            <p className="text-snow text-xl pb-32 pt-8">
-              este un proiect realizat de{' '}
-              <a title={Data.footer.links.asociatia.title} href={Data.footer.links.asociatia.url}>
-                {Data.footer.links.asociatia.text}
-              </a>{' '}
-              cu <br className="hidden sm:block" />
-              sprijinul <a href="https://www.omvpetrom.com/ro">OMV Petrom</a> prin programul{' '}
-              <a title="Țara lui Andrei" href="https://taraluiandrei.ro/">
-                Țara lui Andrei
-              </a>
-              .
+        <div className="section">
+          <div className="interior md:flex md:flex-wrap md:justify-between xl:max-w-grid xl:m-auto xl:justify-around">
+            <Separator color="celeste" />
+            <div className="mt-8 md:mt-12">
+              <Link>
+                <img alt="Harta Serviciilor Sociale Logo" src={logoLight} />
+              </Link>
+              <p className="text-snow text-xl pb-32 pt-8 xl:pt-0">
+                este un proiect realizat de{' '}
+                <a title={Data.footer.links.asociatia.title} href={Data.footer.links.asociatia.url}>
+                  {Data.footer.links.asociatia.text}
+                </a>{' '}
+                cu <br className="hidden sm:block" />
+                sprijinul <a href="https://www.omvpetrom.com/ro">OMV Petrom</a> prin programul{' '}
+                <a title="Țara lui Andrei" href="https://taraluiandrei.ro/">
+                  Țara lui Andrei
+                </a>
+                .
+              </p>
+            </div>
+            <Navigation navClass="footer-nav hidden sm:block xl:content-end md:mt-12" nav={Data.page} />
+            <p className="border-brown border-t-2 text-center text-lightbrown py-2 md:w-full">
+              {Data.footer.texts.copyright}
+              {new Date().getFullYear()}
             </p>
           </div>
-          <nav className="hidden sm:block">
-            <ul>
-              <li>
-                <Link title="Harta" to="…">
-                  Harta
-                </Link>
-              </li>
-              <li>
-                <Link title="Harta" to="…">
-                  Despre noi
-                </Link>
-              </li>
-              <li>
-                <Link title="Harta" to="…">
-                  Noutăți
-                </Link>
-              </li>
-              <li>
-                <Link title="Harta" to="…">
-                  Parteneri
-                </Link>
-              </li>
-              <li>
-                <Link title="Harta" to="…">
-                  Statistici
-                </Link>
-              </li>
-              <li>
-                <Link title="Harta" to="…">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </nav>
-
-          <p className="border-brown border-t-2 text-center text-lightbrown py-2 md:w-full">
-            {Data.footer.texts.copyright}
-            {new Date().getFullYear()}
-          </p>
         </div>
       </footer>
-    )
+    );
   }
 }
