1990155e6aaa966a1517ca7e860825e054d9dcb9
Catalin Secosan <secosan@gmail.com>
2020-01-09 19:13:20 +0200

    added desktop options to section3 2 and hero

--- src/components/pages/HomePage/Sections/Section3.tsx
+++ src/components/pages/HomePage/Sections/Section3.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import CircularProgressBar from '../../../CircularProgressBar'
-import ProgressBar from '../../../ProgressBar'
+import React from 'react';
+import CircularProgressBar from '../../../CircularProgressBar';
+import ProgressBar from '../../../ProgressBar';
+import GradientMap from '../../../GradientMap';
 
 export default class Content extends React.Component {
   render() {
     return (
       <div id="section3" className="section circularprogressbar">
-        <div className="interior bg-snow py-8 mb-16">
+        <div className="bg-snow py-8 mb-16">
           <div className="md:flex">
-            <CircularProgressBar class="md:w-1/2" publice={1396} private={1154} />
-            <ProgressBar class="md:w-1/2" adapost={21} copii={538} />
+            <CircularProgressBar class="md:w-1/2 xl:w-1/3" public={1396} private={1154} />
+            <ProgressBar class="hidden md:block md:w-1/2 xl:w-1/3 xl:m-auto" shelters={21} skids={538} />
+            <GradientMap class="hidden xl:block xl:w-1/3 xl:m-auto" />
           </div>
-          <button className="my-1 absolute md:left-1/4">Toate statisticile</button>
+          <button className="section-button my-1">Toate statisticile</button>
         </div>
       </div>
-    )
+    );
   }
 }
