04c6a7482252598b109251eed74481f881887051
Catalin Secosan <secosan@gmail.com>
2020-01-10 14:57:24 +0200

    refactored to functional component and renamed class

--- src/components/Separator/index.tsx
+++ src/components/Separator/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 type Props = {
   color: string;
-  class: string;
+  classSeparator: string;
   button: string;
   top: string;
   height: string;
@@ -10,21 +10,22 @@ type Props = {
 {
   /*@todo Seco -> it must be redeclare to Separator parameters*/
 }
-export default class Separator extends React.Component {
-  render() {
-    return (
-      <div
-        className={`${this.props.class} separator bg-${this.props.color} h-${this.props.height}`}
-        style={{ bottom: this.props.bottom, top: this.props.top }}></div>
-    );
-  }
+
+function Separator(props: Props) {
+  return (
+    <div
+      className={`${props.classSeparator} separator bg-${props.color} h-${props.height}`}
+      style={{ bottom: props.bottom, top: props.top }}></div>
+  );
 }
 
 Separator.defaultProps = {
-  class: '',
+  classSeparator: '',
   height: 's',
   width: '100%',
   color: 'black',
   bottom: '',
   top: '',
 };
+
+export default Separator;
