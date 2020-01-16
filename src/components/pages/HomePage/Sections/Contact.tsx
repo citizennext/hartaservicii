c51330680685fa30a405d3f15835e3cf2b5f1670
Catalin Secosan <secosan@gmail.com>
2020-01-16 15:57:15 +0200

    fixed small artefacts

--- src/components/pages/HomePage/Sections/Contact.tsx
+++ src/components/pages/HomePage/Sections/Contact.tsx
@@ -7,7 +7,7 @@ export default class Contact extends React.Component {
     return (
       <div
         id="contact"
-        className="section blogposts bg-white pt-12 md:pt-24 md:mt-40 md:pb-32 xl:max-w-gridt xl:m-auto xl:pt-24 xl:mt-40 xl:pb-48">
+        className="section blogposts bg-white pt-12 md:pt-24 md:mt-40 md:pb-32 md:m-auto md:max-w-gridt xl:max-w-gridt xl:m-auto xl:pt-24 xl:mt-40 xl:pb-48">
         <div className="interior md:flex">
           <div className="contact-left md:w-1/2 md:mr-2">
             <h3 className="wide pl-8">Hai alături de noi!</h3>
@@ -15,7 +15,7 @@ export default class Contact extends React.Component {
               <img className="illu-logo" src={Illu} />
               <img className="no-type" src={Notype} />
             </div>
-            <button className="social">Comunitate</button>
+            <button className="social md:ml-6">Comunitate</button>
           </div>
           <div className="contact-right mb-32 md:w-1/2 md:mb-0 md:ml-2 md:self-end">
             <p className="py-8">
