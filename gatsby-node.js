6b7640553bd3717a0f8a37ab53f10ae5837af075
Cezar Neaga <hi@cezarneaga.eu>
2020-01-17 11:43:15 +0100

    hotfix - add graphql codegen for ts types

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,40 +1,21 @@
-// @ts-ignore
-const write = require('write')
-// @ts-ignore
-const { introspectionQuery, graphql, printSchema } = require('gatsby/graphql')
+// const write = require('write');
+// const { introspectionQuery, graphql, printSchema } = require('gatsby/graphql');
 
-/**
- * Generate GraphQL schema.json file to be read by tslint
- * Thanks: https://gist.github.com/kkemple/6169e8dc16369b7c01ad7408fc7917a9
- */
-exports.onPostBootstrap = async ({ store }) => {
-  try {
-    const { schema } = store.getState()
-    const jsonSchema = await graphql(schema, introspectionQuery)
-    const sdlSchema = printSchema(schema)
+// /**
+//  * Generate GraphQL schema.json file to be read by tslint
+//  * Thanks: https://gist.github.com/kkemple/6169e8dc16369b7c01ad7408fc7917a9
+//  */
+// exports.onPostBootstrap = async ({ store }) => {
+//   try {
+//     const { schema } = store.getState();
+//     const jsonSchema = await graphql(schema, introspectionQuery);
+//     const sdlSchema = printSchema(schema);
 
-    write.sync('schema.json', JSON.stringify(jsonSchema.data), {})
-    write.sync('schema.graphql', sdlSchema, {})
+//     write.sync('schema.json', JSON.stringify(jsonSchema.data), {});
+//     write.sync('schema.graphql', sdlSchema, {});
 
-    console.log('\n\n[gatsby-plugin-extract-schema] Wrote schema\n') // eslint-disable-line
-  } catch (error) {
-    console.error(
-      '\n\n[gatsby-plugin-extract-schema] Failed to write schema: ',
-      error,
-      '\n'
-    )
-  }
-}
-
-const { createFilePath } = require(`gatsby-source-filesystem`)
-exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    })
-  }
-}
+//     console.log('\n\n[gatsby-plugin-extract-schema] Wrote schema\n'); // eslint-disable-line
+//   } catch (error) {
+//     console.error('\n\n[gatsby-plugin-extract-schema] Failed to write schema: ', error, '\n');
+//   }
+// };
