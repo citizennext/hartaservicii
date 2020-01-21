faf1efad2ec14787c80866f74cdc6e2083447993
Catalin Secosan <secosan@gmail.com>
2020-01-21 16:50:30 +0200

    first commit blog post - Stefan will add toast

--- src/components/pages/Noutati/AfterHeader.tsx
+++ src/components/pages/Noutati/AfterHeader.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Img from 'gatsby-image';
+import Separator from '../../Separator';
 
-export function AfterHeader() {
+type Props = {
+  header: object;
+};
+
+export function AfterHeader({ header }: Props) {
   return (
-    <div className="page-header">
-      <div className="wrapper" />
+    <div className="page-header blog-single">
+      <Separator color="burg" />
+      <div className="wrapper">
+        <Img fluid={header} />
+      </div>
+      <Separator color="burg" bottom="-6" />
     </div>
   );
 }
--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,17 +1,47 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 import Seo from '../components/Seo';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { AfterHeader } from '../components/pages/AboutUs/AfterHeader';
+import { AfterHeader } from '../components/pages/Noutati/AfterHeader';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import Separator from '../components/Separator';
 
 export default ({ data }: any) => {
   const { blog } = data.hasura;
+  const windowurl = window.location.href;
   return (
     <>
       <Seo postTitle={blog.title} isRepeatable={true} postImage={blog.image.url} summary={blog.summary} />
       <Header />
-      <AfterHeader header={blog.header} />
+      <AfterHeader header={blog.image.urlSharp.childImageSharp.fluid} />
+      <div className="blog-single section interior">
+        <h1>{blog.title}</h1>
+        <span className="date">
+          {blog.publishedAt.toLocaleString('ro-RO', {
+            timeZone: 'UTC',
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+          })}
+        </span>
+        <div className="blog-content" dangerouslySetInnerHTML={{ __html: blog.content.html }} />
+        <div className="prev-next-buttons">
+          <Link>
+            <button className="prev-button"></button>
+          </Link>
+          <Link>
+            <button className="next-button"></button>
+          </Link>
+          <Link>
+            <button className="read-more">Citeste si alte articole</button>
+          </Link>
+        </div>
+        <Separator color="snow" width="100px" />
+        <CopyToClipboard text={windowurl}>
+          <button className="share-button">Distribuie</button>
+        </CopyToClipboard>
+      </div>
       <Footer />
     </>
   );
