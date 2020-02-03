dd1d18b4bc194b776e461029ba49c3244a187a19
St3phan <office@green-art.ro>
2020-02-03 11:39:29 +0200

    #10: added files after merge of rest

---
+++ src/components/Header/Logo.tsx
@@ -0,0 +1,9 @@
+/** @jsx jsx */
+import React from 'react';
+import { jsx } from '@emotion/core';
+
+export default class Logo extends React.Component {
+  render() {
+    return <div>Logo</div>;
+  }
+}
