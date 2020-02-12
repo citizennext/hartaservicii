fd9f770ca1ded7f4e8f1af00f10d297ea71dd355
Catalin Secosan <secosan@gmail.com>
2020-02-12 10:12:45 +0200

    hidden separator from DOM, not CSS

--- src/components/AfterHeader.tsx
+++ src/components/AfterHeader.tsx
@@ -13,12 +13,12 @@ type Props = {
 export function AfterHeader({ header, image, className, hasTopSeparator, hasBottomSeparator }: Props) {
   return (
     <div className={`page-header ${className}`}>
-      <Separator classSeparator="blog-only" color="burg" />
+      {hasTopSeparator && <Separator color="burg" />}
       <div className="wrapper">
         <span>{header}</span>
         {image && <Img fluid={image} />}
       </div>
-      <Separator classSeparator="blog-only" color="burg" bottom="-6" />
+      {hasBottomSeparator && <Separator color="burg" bottom="-6" />}
     </div>
   );
 }
