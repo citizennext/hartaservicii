390407b122e8288e0e8dc1887b15fa1d9c048a64
Sabau Cristina <ms@codesicle.com>
2020-02-12 16:17:41 +0200

    fetch filtered providers with apollo

---
+++ src/apollo/wrapRootElement.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { client } from './client';
+
+export const wrapRootElement = ({ element }) => <ApolloProvider client={client}>{element}</ApolloProvider>;
