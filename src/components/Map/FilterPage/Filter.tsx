0ab1fbf3f79194bf8d8c09cf5dcd9f4280474967
Sabau Cristina <ms@codesicle.com>
2020-02-14 12:55:54 +0200

    adding all filter options fetch data in map component

--- src/components/Map/FilterPage/Filter.tsx
+++ src/components/Map/FilterPage/Filter.tsx
@@ -4,7 +4,7 @@ import Drawer from '../../Drawer/Drawer';
 import { DrawerContext } from '../../Drawer/DrawerContext';
 import { useWindowSize } from '../../../hooks/useWindowSize';
 import { FilterMenu } from '../../Icons';
-// import providerData from '../../../data/providers-data.json';
+import options from '../../../data/filter-options.json';
 
 // type OptionType = { [key: string]: any }
 // type OptionsType = Array<OptionType>
@@ -18,16 +18,6 @@ type Props = {
 };
 
 const Filter: React.FC<Props> = props => {
-  const options = [
-    { "value": null, "label": "Toate categoriile de servicii" },
-    { "value": "1", "label": "Centru rezidenţial cu cazare pe perioadă nedeterminată" },
-    { "value": "2", "label": "Centru rezidenţial cu cazare pe perioadă determinată" },
-    { "value": "3", "label": "Locuinţă protejată" },
-    { "value": "4", "label": "Centru de zi" },
-    { "value": "5", "label": "Unitate de îngrijiri la domiciliu" },
-    { "value": "6", "label": "Serviciu acordat în comunitate" }
-];
-
   // @ts-ignore
   const { state, dispatch } = useContext(DrawerContext);
   const openDrawer = () => {
@@ -35,21 +25,24 @@ const Filter: React.FC<Props> = props => {
   };
   const windowSize = useWindowSize();
 
-  // function handleChangeAge(newValue: any) {
-  //   props.onFilterChange({ ageValue: newValue });
-  // }
+  // const filters = props.filters
 
-  function handleChangeService(newValue: any) {
-    props.onFilterChange({service: newValue.value})
+  function handleChangeAge(newValue: any) {
+    props.onFilterChange({ ageValue: `%${newValue.value}%` });
   }
 
-  /*function handleChangeSpecialization(newValue: any, actionMeta: any) {
-    props.onFilterChange({ageValue: newValue})
-  }*/
-  /*function handleChangeSupplierType(newValue: any, actionMeta: any) {
-    props.onFilterChange({ageValue: newValue})
-  }*/
-  // console.log(props.filters.ageValue);
+  function handleChangeService(newValue: any) {
+    props.onFilterChange({service: parseInt(newValue.value)})
+  }
+
+  function handleChangeSpecialization(newValue: any) {
+    props.onFilterChange({ageValue: `%${newValue.value}%`})
+  }
+
+  function handleChangeSupplierType(newValue: any) {
+    props.onFilterChange({supplierPrivate: newValue.value})
+  }
+
   return (
     <>
       {props.drawer && windowSize.width && windowSize.width < 768 && (
@@ -64,10 +57,10 @@ const Filter: React.FC<Props> = props => {
             toggleHandler={openDrawer}
             closeButton={<FilterMenu className="close" size={20} />}>
             <div className="select-options">
-              {/* <Select options={options} onChange={handleChangeAge} /> */}
-              <Select options={options} onChange={handleChangeService}/>
-              {/*<Select value={specializationValue} options={options[2]} onChange={handleChangeSpecialization}/>
-              <Select value={supplierTypeValue} options={options[3]} onChange={handleChangeSupplierType}/> */}
+              <Select options={options.age} onChange={handleChangeAge} />
+              <Select options={options.service_type} onChange={handleChangeService}/>
+              <Select options={options.specializari} onChange={handleChangeSpecialization}/>
+              <Select options={options.accesibilitate} onChange={handleChangeSupplierType}/>
             </div>
           </Drawer>
         </div>
--- src/components/pages/Harta/Content.tsx
+++ src/components/pages/Harta/Content.tsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import FilterResults from '../../Map/FilterPage/FilterResults';
+import Providers from '../../Map/index';
 import { DrawerProvider } from '../../Drawer/DrawerContext';
 
 export default class Harta extends Component {
   render() {
     return (
     <DrawerProvider>
-      <FilterResults />
+      <Providers />
     </DrawerProvider>);
   }
 }
