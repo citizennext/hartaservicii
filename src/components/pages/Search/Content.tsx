9aa507b5d4987082f80ddefb6870a42d4c32832c
Catalin Secosan <secosan@gmail.com>
2020-02-14 12:17:50 +0200

    added sass and started structure

--- src/components/pages/Search/Content.tsx
+++ src/components/pages/Search/Content.tsx
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 export default class SearchContent extends Component {
   render() {
-    return <div></div>;
+    return (
+      <>
+        <input
+          id="search"
+          className="border-burg border-6 rounded-full background-white absolute -mt-24 pl-10"
+          placeholder="Caută serviciu, zonă sau tipologie beneficiar"
+        />
+      </>
+    );
   }
 }
