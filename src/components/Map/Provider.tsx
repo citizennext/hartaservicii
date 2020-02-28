97ea7e36e00484f3c46b1b638aec10ca6046a1ac
Catalin Secosan <secosan@gmail.com>
2020-02-28 17:21:28 +0200

    cleaned up sass - wip weird scroll on mobile

--- src/components/Map/Provider.tsx
+++ src/components/Map/Provider.tsx
@@ -3,12 +3,13 @@ import { Link } from 'gatsby';
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import gql from 'graphql-tag';
 import { useMutation, useQuery } from '@apollo/react-hooks';
-import iconClose from '../../assets/images/icon_arrowg.svg';
 import hssLogo from '../../assets/images/icon_HSS_symbolleaf.svg';
 import StarRatingComponent from 'react-star-rating-component';
-import iconShare from '../../assets/images/icon_share.svg';
 import iconDirections from '../../assets/images/icon_directions.svg';
+import iconClose from '../../assets/images/icon_arrowg.svg';
 import Leaflet, { LatLngTuple } from 'leaflet';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import { NotificationManager } from 'react-notifications';
 
 function Provider(props: any) {
   const ratingMutation = gql`
@@ -50,6 +51,7 @@ function Provider(props: any) {
           decision_no
           supplier_type {
             name
+            private
           }
         }
         service {
@@ -66,10 +68,8 @@ function Provider(props: any) {
     variables: { provider },
   });
   const [addRating] = useMutation(ratingMutation);
-
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error! ${error.message}</p>;
-
   const providers = data.providers_by_pk;
   const averageRating = (providers.rating_aggregate.aggregate.avg.rating / 10).toFixed(1);
   const percentageRating = (providers.rating_aggregate.aggregate.avg.rating * 100) / 50;
@@ -137,7 +137,7 @@ function Provider(props: any) {
                 Directii
                 {providers.coordinates ? (
                   <a
-                    href={'https://www.google.ro/maps/search/' + providers.coordinates + ',16z'}
+                    href={'https://www.google.ro/maps/search/' + providers.coordinates}
                     target="_blank"
                     rel="noopener noreferrer">
                     <img src={iconDirections} />
@@ -148,9 +148,11 @@ function Provider(props: any) {
               </p>
               <p>
                 Distributie
-                <a href="#">
-                  <img src={iconShare} />
-                </a>{' '}
+                <CopyToClipboard text={location.href}>
+                  <button
+                    className="share-button"
+                    onClick={() => NotificationManager.success('Link copiat în clipboard!')}></button>
+                </CopyToClipboard>
               </p>
             </div>
             <div className="pin-address">
@@ -163,23 +165,38 @@ function Provider(props: any) {
             <div className="pin-phone">
               <p>Telefon</p>
               <span>{providers.phone ? providers.phone : '-'}</span>
-              <span>{providers.phone ? providers.phone : '-'}</span>
             </div>
             <div className="pin-web">
               <p>Web</p>
-              <a href="#">- </a>
+              <span>-</span>
             </div>
             <div className="pin-services">
-              <p>Servicii</p>
-              <ul>
-                <li>{providers.service.name ? providers.service.name : '-'}</li>
-              </ul>
+              <p>Tip / cod serviciu </p>
+              <span>
+                {providers.service.name} ({providers.service.code})
+              </span>
+              <div className="pin-license">
+                <div>
+                  <p>Dată licență </p>
+                  <span>
+                    {providers.license_date_5years ? providers.license_date_5years : providers.license_date_provisional}
+                  </span>
+                </div>
+                <div>
+                  <p>Instituția care a eliberat licența </p>
+                  <span>{providers.license_by ? providers.license_by : '-'}</span>
+                </div>
+              </div>
             </div>
           </div>
           <div className="pin-provider">
             <div>
               <p>Administrator: </p>
-              <p>Privat</p>
+              <p>{providers.supplier.supplier_type.private ? 'Privat' : 'Public'}</p>
+            </div>
+            <div>
+              <p>Tip furnizor: </p>
+              <p>{providers.supplier.supplier_type.name}</p>
             </div>
             <div>
               <p>Nume furnizor: </p>
@@ -190,36 +207,16 @@ function Provider(props: any) {
               <p>{providers.supplier.cui_cif ? providers.supplier.cui_cif : '-'}</p>
             </div>
             <div>
-              <p>Acreditat: </p>
-              <p>Da</p>
-            </div>
-            <div>
               <p>Număr decizie: </p>
-              <p>TOJ 5669213</p>
+              <p>{providers.supplier.decision_no ? providers.supplier.decision_no : '-e'}</p>
             </div>
             <div>
               <p>Dată decizie: </p>
-              <p>{providers.license_date ? providers.license_date : '- '}</p>
+              <p>{providers.supplier.decision_date ? providers.supplier.decision_date : '-'}</p>
             </div>
             <div>
               <p>Serie și număr certificat: </p>
               <p>{providers.supplier.certificate_serial_no ? providers.supplier.certificate_serial_no : '-'}</p>
-            </div>
-            <div>
-              <p>Cod serviciu social: </p>
-              <p>GH 5698133</p>
-            </div>
-            <div>
-              <p>Dată licență provizorie: </p>
-              <p>{providers.license_date_provisional ? providers.license_date_provisional : '-'}</p>
-            </div>
-            <div>
-              <p>Număr și serie licență de funcționare: </p>
-              <p>{providers.license_no ? providers.license_no : '-'}</p>
-            </div>
-            <div>
-              <p>Instituția care a eliberat licența: </p>
-              <p>{providers.license_by ? providers.license_by : '-'}</p>
             </div>
           </div>
           <footer>
