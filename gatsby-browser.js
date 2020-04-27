22c9c90c32d86165dee2442c518f6636c920f0ef
Cezar Neaga <hi@cezarneaga.eu>
2020-04-27 22:37:37 +0200

    add amplify stack and test login

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,26 @@
 export { wrapRootElement } from './src/apollo/wrapRootElement';
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+// You can delete this file if you're not using it
+
+import Auth from '@aws-amplify/auth';
+import { setUser } from './src/utils/auth';
+
+export const onRouteUpdate = (state, page, pages) => {
+  Auth.currentAuthenticatedUser()
+    .then((user) => {
+      const userInfo = {
+        ...user.attributes,
+        username: user.username,
+        token: user.signInUserSession.idToken.jwtToken,
+      };
+      setUser(userInfo);
+    })
+    .catch((err) => {
+      window.localStorage.setItem('gatsbyUser', JSON.stringify({}));
+    });
+};
