3a04bf68387ed4f693d53e43d1b1ee868ef0e517
Cezar Neaga <hi@cezarneaga.eu>
2021-12-08 08:39:37 +0100

    update config

--- site-config.js
+++ site-config.js
@@ -1,14 +1,14 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   siteTitle: 'Harta Serviciilor Sociale din România',
   siteTitleShort: 'HSSR',
   siteDescription: 'Îngrijim și suntem îngrijiți mai bine',
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'https://serviciisociale.ro',
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/assets/images/harta-icon.png'),
   social: {
-    twitter: 'cezarneaga',
-    fbAppId: '123',
+    twitter: 'citizennextro',
+    fbAppId: '230938941312776',
   },
-}
+};
